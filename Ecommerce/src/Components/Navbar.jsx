9fc819f2190b9eb251e6e22e322881febff084c4
MoatazEssam <120606709+MoatazEssam@users.noreply.github.com>
2024-07-28 03:16:53 +0300

    Update Navbar.jsx

--- Ecommerce/src/Components/Navbar.jsx
+++ Ecommerce/src/Components/Navbar.jsx
@@ -7,7 +7,10 @@ const Navbar = () => {
       <div className="bg-primary/40 py-2">
         <div className="container flex justify-between items-center">
           <div>
-            <a href="#" className="font-bold text-2xl sm:text-3xl flex gap-2">
+            <a
+              href="#"
+              className="font-bold text-2xl sm:text-3xl items-end flex gap-2"
+            >
               <img src={logo} alt="logo" className="w-10 uppercase" />
               Fa
             </a>
@@ -16,8 +19,8 @@ const Navbar = () => {
             <div>
               <input
                 type="text"
-                placeholder="search"
-                className="w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1
+                placeholder="Search"
+                className="w-[200px] hover:border-black group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1
                 focus:outline-none focus:border-1 focus:border-primary"
               />
             </div>
