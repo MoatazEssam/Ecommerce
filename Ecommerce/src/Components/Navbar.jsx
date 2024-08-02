a3b55aa18db0f6a0a2db0e2cb47ffb4c2e44e612
MoatazEssam <120606709+MoatazEssam@users.noreply.github.com>
2024-08-02 04:01:36 +0300

    add darkmode and finish appbar

--- Ecommerce/src/Components/Navbar.jsx
+++ Ecommerce/src/Components/Navbar.jsx
@@ -2,9 +2,54 @@ import React from "react";
 import logo from "../assets/clothes-shop.png";
 import { GoSearch } from "react-icons/go";
 import { FaShoppingCart } from "react-icons/fa";
+import { FaCaretDown } from "react-icons/fa";
 import DarkMode from "./DarkMode";
 
 const Navbar = () => {
+  const menu = [
+    {
+      id: 1,
+      name: "Home",
+      link: "/#",
+    },
+    {
+      id: 2,
+      name: "Top Rated",
+      link: "/#",
+    },
+    {
+      id: 3,
+      name: "Kids",
+      link: "/#",
+    },
+    {
+      id: 4,
+      name: "Mens",
+      link: "/#",
+    },
+    {
+      id: 3,
+      name: "Womens",
+      link: "/#",
+    },
+  ];
+  const dropdownmenu = [
+    {
+      id: 1,
+      name: "Trending Products",
+      link: "/#",
+    },
+    {
+      id: 2,
+      name: "best sellers",
+      link: "/#",
+    },
+    {
+      id: 3,
+      name: "Top Rated",
+      link: "/#",
+    },
+  ];
   return (
     <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 z-40 relative">
       <div className="bg-primary/40 py-2">
@@ -45,7 +90,45 @@ const Navbar = () => {
         </div>
       </div>
       {/* //////////////////// */}
-      <div></div>
+      <div className="flex justify-center">
+        <ul className="sm:flex hidden items-center gap-4">
+          {menu.map((data) => (
+            <li key={data.id}>
+              <a
+                href={data.link}
+                className="inline-block px-4 duration-200 transition-all hover:text-primary"
+              >
+                {data.name}
+              </a>
+            </li>
+          ))}
+          <li className="group relative cursor-pointer">
+            <a className="flex  items-center gap-[2px] py-2" href="/#">
+              Trending Items
+              <span>
+                <FaCaretDown className="transition-all duration-200 group-hover:rotate-180" />
+              </span>
+            </a>
+            <div className="absolute hideen z-[100] shadow-lg hidden w-[200px] rounded-md bg-white p-2 text-black group-hover:block">
+              <ul>
+                {dropdownmenu.map((data) => (
+                  <li
+                    className="hover:text-primary transition-all duration-200"
+                    key={data.id}
+                  >
+                    <a
+                      href={data.link}
+                      className="inline-block p-2 w-full rounded-md hover:bg-primary/40"
+                    >
+                      {data.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
